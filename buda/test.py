3bfe4e7b7c8fc1c8fe583cbe8754067ff5c0f6a9
mgwinst <matteospatola@hotmail.com>
2025-05-22 06:20:25 +0000

    clean slate for blackhole

--- buda/test.py
+++
@@ -1,9 +0,0 @@
-import pybuda
-import torch
-
-tt0 = pybuda.TTDevice("tt0")
-cpu0 = pybuda.CPUDevice("cpu0")
-tt0.place_module(pybuda.PyTorchModule("linear", torch.nn.Linear(64, 64)))
-
-print(tt0, cpu0)
-
